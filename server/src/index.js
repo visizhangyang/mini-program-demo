1e686f99db88cb552e04301dd7afd866a0e9dbbe
xiao11lang <1023819715@qq.com>
2018-11-25 20:13:13 +0800

    小程序服务端

--- server/src/index.js
+++ server/src/index.js
@@ -14,43 +14,44 @@ connection.connect()
 app.post('/api/login', function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        var userInfo=fields.userInfo;
+        var userInfo = fields.userInfo;
         if (err) {
             console.log(err)
         } else {
             var url = `https://api.weixin.qq.com/sns/jscode2session?appid=wxbeb053de6b55dda0&secret=a3ca8922801e4dbedbb430b94580f8a0&js_code=${fields.code}&grant_type=authorization_code`
             axios.get(url).then(function (resp) {
-                var sql='select count(*) count from userInfo where openid=?'
-                connection.query(sql,[resp.data.openid],function(err,result){
-                    if(result[0].count==0){
-                        var sql='insert into userInfo (openid,nickName,gender,city,province,country,avatarUrl) values(?,?,?,?,?,?,?)';
-                        connection.query(sql,[resp.data.openid,userInfo.nickName,userInfo.gender,userInfo.city,userInfo.province,userInfo.country,userInfo.avatarUrl],function(err,result){
-                            if(err){
+                var sql = 'select count(*) count from userInfo where openid=?'
+                connection.query(sql, [resp.data.openid], function (err, result) {
+                    if (result[0].count == 0) {
+                        var sql = 'insert into userInfo (openid,nickName,gender,city,province,country,avatarUrl) values(?,?,?,?,?,?,?)';
+                        connection.query(sql, [resp.data.openid, userInfo.nickName, userInfo.gender, userInfo.city, userInfo.province, userInfo.country, userInfo.avatarUrl], function (err, result) {
+                            if (err) {
                                 console.log(err)
-                            }else{
+                            } else {
                                 res.json({
                                     third_Session: resp.data.session_key + '_logined_' + resp.data.openid
                                 })
                             }
                         })
-                    }else{
+                    } else {
                         res.json({
                             third_Session: resp.data.session_key + '_logined_' + resp.data.openid
                         })
                     }
-                    
+
                 })
-                
+
             })
         }
     });
 })
+
 app.get('/api/getLove', function (req, res) {
-    var sql='select * from love'
-    connection.query(sql,function(err,result){
-        if(err){
+    var sql = 'select * from love'
+    connection.query(sql, function (err, result) {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             res.json(result)
         }
     })
@@ -58,14 +59,162 @@ app.get('/api/getLove', function (req, res) {
 app.post('/api/writeLove', function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        var params=[fields.nickName,fields.gender,fields.avatarUrl,fields.toWho
-        ,fields.content,fields.watchCount,fields.commentCount,fields.thumpCount];
-        var sql='insert into love (nickName,gender,avatarUrl,toWho,content,watchCount,commentCount,thumpCount) values(?,?,?,?,?,?,?,?)';
-        connection.query(sql,params,function(err,result){
-            if(err){
+        var params = [fields.openid,fields.nickName, fields.gender, fields.avatarUrl, fields.toWho, fields.content,
+             fields.watchCount, fields.commentCount, fields.thumpCount,fields.writeTime];
+        var sql = 'insert into love (openid,nickName,gender,avatarUrl,toWho,content,watchCount,commentCount,thumpCount,writeTime) values(?,?,?,?,?,?,?,?,?,?)';
+        connection.query(sql, params, function (err, result) {
+            if (err) {
                 console.log(err)
-            }else{
+            } else {
                 res.json('success')
+            }
+        })
+    });
+})
+app.post('/api/addLoveVisitor', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var params = [fields.nickName, fields.avatarUrl, fields.id];
+        var sql = 'insert into love_visitor (nickName,avatarUrl,id) values(?,?,?)';
+        connection.query(sql, params, function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json('success')
+            }
+        })
+    });
+})
+app.post('/api/getLoveVisitor', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var sql = 'select distinct nickName,avatarUrl from love_visitor where id=?'
+        connection.query(sql, [fields.id], function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(result)
+            }
+        })
+    });
+
+})
+app.post('/api/addLoveComment', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var params = [fields.nickName, fields.avatarUrl, fields.id, fields.content, fields.commentTime];
+        var sql = 'insert into love_comment (nickName,avatarUrl,id,content,commentTime) values(?,?,?,?,?)';
+        connection.query(sql, params, function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json('success')
+            }
+        })
+    });
+})
+app.post('/api/getLoveComment', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var sql = 'select * from love_comment where id=?'
+        connection.query(sql, [fields.id], function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(result)
+            }
+        })
+    });
+
+})
+app.post('/api/addThumpCount',function(req,res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var sql = 'update publish_love set thumpCount=?  where id=?'
+        connection.query(sql, [fields.count,fields.id], function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(result)
+            }
+        })
+    });
+})
+
+app.get('/api/getAppoint', function (req, res) {
+    var sql = 'select * from appoint'
+    connection.query(sql, function (err, result) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(result)
+        }
+    })
+})
+app.post('/api/writeAppoint', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var params = [fields.openid,fields.nickName, fields.avatarUrl, fields.theme, fields.want,
+             fields.detail, fields.appointTime, fields.appointLocation,fields.writeTime];
+        var sql = 'insert into appoint (openid,nickName,avatarUrl,theme,want,detail,appointTime,appointLocation,writeTime) values(?,?,?,?,?,?,?,?,?)';
+        connection.query(sql, params, function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json('success')
+            }
+        })
+    });
+})
+app.post('/api/getMes', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var sql = 'select * from mes where toWho=?'
+        connection.query(sql, [fields.id], function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(result)
+            }
+        })
+    });
+})
+app.post('/api/addMes', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var params = [fields.nickName, fields.avatarUrl, fields.toWho, fields.content,fields.writeTime];
+        var sql = 'insert into mes (nickName,avatarUrl,toWho,content,writeTime) values(?,?,?,?,?)';
+        connection.query(sql, params, function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json('success')
+            }
+        })
+    });
+})
+app.post('/api/getMyLove', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var sql = 'select * from love where openid=?'
+        connection.query(sql, [fields.id], function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(result)
+            }
+        })
+    });
+})
+app.post('/api/getMyAppoint', function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var sql = 'select * from appoint where openid=?'
+        connection.query(sql, [fields.id], function (err, result) {
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(result)
             }
         })
     });
